919d645213340abd78e647e0b139754b2fc6e2dd
essi-martinss <essi_dalleth@outlook.com>
2025-02-12 11:55:13 -0300

    Exercicios [12-02-2025]

--- src/com/agibank/Metodos1102/ExercicioMetodoDesvioPadrão1102.java
+++ src/com/agibank/Metodos1102/ExercicioMetodoDesvioPadrão1102.java
@@ -5,18 +5,29 @@ package com.agibank.Metodos1102;
 public class ExercicioMetodoDesvioPadrão1102 {
 
     public static void main(String[] args) {
-
-        double[] valores = {100.0, 102.0, 101.0, 103.0, 105.0};
-        int intervalo = 3;
-
-        double[] medias =  calcularMediaMovel(valores, intervalo);
-
-
+        double[] valores = {0.01, 0.02, -0.01, 0.03, -0.02};
+        double desvioPadraoResult =  calcularDesvioPadrao(valores);
+        System.out.printf(" %.4f", desvioPadraoResult);
     }
-    public  static double calcularMediaMovel(double [] valores, int intervalo){
-
-
-        return 0;
+    public  static double calcularDesvioPadrao(double[] valores){
+        double formulaDiferencaEAoQuadrado = 0;
+        double result = 0;
+                
+        for (int i = 0; i < valores.length ; i++) {
+             formulaDiferencaEAoQuadrado += Math.pow (valores[i] - calcularMedia(valores),2);
+            double div = formulaDiferencaEAoQuadrado /valores.length ;  // divisão da soma da diferença do quadrado.
+            result = Math.sqrt(div);  // raiz quadrada do resultado;
+        }
+        return  result;
+    }
+    public  static double calcularMedia(double [] valores){
+        double resultMedia = 0;
+        double soma = 0;
+        for (int i = 0; i < valores.length; i++) {
+            soma +=  valores[i];
+            resultMedia = soma / valores.length;
+        }
+        return resultMedia;
     }
 }
 
