afda3aec0b0563e8c086f7f6612c0ec3692ae50a
essi-martinss <essi_dalleth@outlook.com>
2025-02-18 09:08:54 -0300

    Adicionado classe Calculadora [18-02-2025]

---
+++ src/com/agibank/AulaGit/AulaGitCalculadora1802.java
@@ -0,0 +1,13 @@
+package com.agibank.AulaGit;
+
+public class AulaGitCalculadora1802 {
+    public static void main(String[] args) {
+
+        System.out.println("Resultado " + soma(5,3));
+
+    }
+
+    public static int soma(int a, int b){
+        return  a+b;
+    }
+}
