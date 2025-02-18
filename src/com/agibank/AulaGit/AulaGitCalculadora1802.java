9b1ee3df01fe0aed0c40098d0a82bbb7bbb80837
essi-martinss <essi_dalleth@outlook.com>
2025-02-18 09:38:36 -0300

    Altdicionando metodo subtração

--- src/com/agibank/AulaGit/AulaGitCalculadora1802.java
+++ src/com/agibank/AulaGit/AulaGitCalculadora1802.java
@@ -4,10 +4,14 @@ public class AulaGitCalculadora1802 {
     public static void main(String[] args) {
 
         System.out.println("Resultado " + soma(5,3));
+        System.out.println("Resultado " + subtracao(5,3));
 
     }
-
     public static int soma(int a, int b){
         return  a+b;
+    }
+
+    public static int subtracao(int a, int b){
+        return  a-b;
     }
 }
